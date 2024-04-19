1a103f5596b828c7f803ed2c1ae88aa7186727d0
SakibAhmedShuva <126283947+SakibAhmedShuva@users.noreply.github.com>
2024-04-19 14:48:01 +0600

    app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def index():
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-'''
+
 if __name__ == "__main__":
     app.run(debug=True)
-    '''
+
